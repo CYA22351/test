f62b45f918b906fc6aed4800cef638d08e23a8f8
bominghan_java <cya3048279304@126.com>
2025-04-27 22:45:49 +0800

    类与对象

---
+++ javaEE/src/Dog.java
@@ -0,0 +1,15 @@
+/**
+ * @author ：陈奕安（3048279304@qq.com）
+ * @date ：Created in 2025/4/27 22:38
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class Dog {
+    public String name;
+    public  int age;
+    public void eat(){
+        System.out.println(name+"吃狗粮");
+    }
+
+}
---
+++ javaEE/src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * @author ：陈奕安（3048279304@qq.com）
+ * @date ：Created in 2025/4/27 22:37
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class Test {
+    public static void main(String[] args) {
+    Dog dog=new Dog();
+    dog.age=1;
+    dog.name="小黑";
+    dog.eat();
+    }
+}
