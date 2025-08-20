dd8a148bfa5cc59a59473fc06b0524df9a9cbacc
bominghan_java <cya3048279304@126.com>
2025-08-20 12:29:39 +0800

    日志分级

--- test_2025_08/后端代码/spring-log-demo/src/main/java/com/cya/springlogdemo/controller/LogController.java
+++ test_2025_08/后端代码/spring-log-demo/src/main/java/com/cya/springlogdemo/controller/LogController.java
@@ -1,5 +1,6 @@
 package com.cya.springlogdemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @modified By：
  * @version:
  */
+@Slf4j//自动生成log对象 q
 @RequestMapping("/log")
 @RestController
 public class LogController {
 
-    private final static Logger logger= LoggerFactory.getLogger(LogController.class);//定义日志对象
+   // private final static Logger logger= LoggerFactory.getLogger(LogController.class);//定义日志对象
 
     @RequestMapping("/print")
     public String print(){
         System.out.println("sout打印日志");
-        logger.info("logger 打印日志");
+        log.info("logger 打印日志");
+        log.trace("trace 打印日志");
+        log.debug("debug 打印日志");
+        log.warn("warn 打印日志");
+        log.error("error 打印日志");
         return "打印日志";
     }
 }
